4c338a85425eac3ab9e44627eb30e175b5834807
Connor Mitchell <37678351+smaybius@users.noreply.github.com>
2024-07-25 00:39:54 -0400

    January 28, 2016 at 2:18 PM

---
+++ CokeCanGen2/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CokeCanGen2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ExportButton = new System.Windows.Forms.Button();
+            this.CanName = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.Picture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Location = new System.Drawing.Point(337, 469);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(163, 39);
+            this.ExportButton.TabIndex = 11;
+            this.ExportButton.Text = "Export as Image";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            this.ExportButton.Click += new System.EventHandler(this.ExportButton_Click_1);
+            // 
+            // CanName
+            // 
+            this.CanName.Font = new System.Drawing.Font("Serif Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CanName.ForeColor = System.Drawing.SystemColors.Window;
+            this.CanName.Location = new System.Drawing.Point(126, 58);
+            this.CanName.Name = "CanName";
+            this.CanName.Size = new System.Drawing.Size(405, 201);
+            this.CanName.TabIndex = 10;
+            this.CanName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CanName.Click += new System.EventHandler(this.CanName_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 469);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 39);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Generate can";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(269, 387);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Enter name here";
+            // 
+            // NameBox
+            // 
+            this.NameBox.AcceptsReturn = true;
+            this.NameBox.Location = new System.Drawing.Point(235, 423);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(185, 26);
+            this.NameBox.TabIndex = 7;
+            this.NameBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.NameBox_KeyDown);
+            this.NameBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Namebox_PreviewKeyDown);
+            // 
+            // Picture
+            // 
+            this.Picture.Image = ((System.Drawing.Image)(resources.GetObject("Picture.Image")));
+            this.Picture.Location = new System.Drawing.Point(-1, -1);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(640, 354);
+            this.Picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Picture.TabIndex = 6;
+            this.Picture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 514);
+            this.Controls.Add(this.ExportButton);
+            this.Controls.Add(this.CanName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.Picture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.Label CanName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.PictureBox Picture;
+
+
+    }
+}
+
---
+++ CokeCanGen2/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace CokeCanGen2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.AcceptButton = button1;
+            CanName.Parent = Picture;
+            CanName.BackColor = Color.Transparent;
+            //PrivateFontCollection pfc = new PrivateFontCollection();
+            //pfc.AddFontFile("CokeFont.ttf");
+            //CanName.Font = new Font(pfc.Families[0], 32, FontStyle.Regular);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BringToFront();
+            this.Focus();
+            //this.KeyPreview = true;
+            GenerateCanText();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            GenerateCanText();
+        }
+
+
+        private void GenerateCanText()
+        {
+            CanName.Text = NameBox.Text;
+            CanName.Font = new Font(CanName.Font.FontFamily, 32 - 0.5f, CanName.Font.Style);
+            while (CanName.Width < System.Windows.Forms.TextRenderer.MeasureText(CanName.Text,
+            new Font(CanName.Font.FontFamily, CanName.Font.Size, CanName.Font.Style)).Width)
+            {
+                CanName.Font = new Font(CanName.Font.FontFamily, CanName.Font.Size - 0.5f, CanName.Font.Style);
+            }
+        }
+
+        private void CanName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExportButton_Click(object sender, EventArgs e)
+        {
+            SaveCokeAsImage();
+        }
+
+        private void SaveCokeAsImage()
+        {
+            using (var bitmap = new Bitmap(Picture.Width, Picture.Height))
+            {
+                Picture.DrawToBitmap(bitmap, Picture.ClientRectangle);
+
+                // Save the screenshot to the specified path that the user has chosen.
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog
+                {
+                    Filter = "Portable Network Graphics|*.png",
+                    Title = "Save your generated can",
+                    FileName = "shareacoke.png"
+                };
+                saveFileDialog1.ShowDialog();
+                bitmap.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        private void NameBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && e.Modifiers == Keys.None)
+            {
+                Button1_Click(sender, e);
+                e.SuppressKeyPress = true;
+            }
+        }
+
+
+        private void Namebox_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && e.Modifiers == Keys.None)
+            {
+                e.IsInputKey = true;
+            }
+        }
+
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            GenerateCanText();
+        }
+
+        private void ExportButton_Click_1(object sender, EventArgs e)
+        {
+            using (var bitmap = new Bitmap(Picture.Width, Picture.Height))
+            {
+                Picture.DrawToBitmap(bitmap, Picture.ClientRectangle);
+
+                // Save the screenshot to the specified path that the user has chosen.
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog
+                {
+                    Filter = "Portable Network Graphics|*.png",
+                    Title = "Save your generated can",
+                    FileName = "shareacoke.png"
+                };
+                saveFileDialog1.ShowDialog();
+                bitmap.Save(saveFileDialog1.FileName);
+            }
+        }
+    }
+}
